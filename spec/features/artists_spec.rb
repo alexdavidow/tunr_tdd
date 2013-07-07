f9320d3b21c0fc5cc6adcc9ab9f6bdc8d7ee0ca7
alexdavidow <alex.davidow@gmail.com>
2013-07-07 19:00:24 -0400

    wrote all tests for index, show, edit and delete. Still have routing errors and ambiguous match errors

--- spec/features/artists_spec.rb
+++ spec/features/artists_spec.rb
@@ -17,10 +17,60 @@ describe 'Artists' do
 
   describe 'list all instances of artist on index view' do
     
-    # I know I have more tests to write, but I wanted to make sure I had the haml code right first.
     it 'displays a table of artist names' do 
       visit artists_path
-      find('th').should have_content('Artist') 
+      save_and_open_page
+      find('th').should have_content('Artist Names')
+      page.should have_selector('table td') 
+    end
+
+    it 'links each artist name to that artists show page' do
+      visit artists_path
+      page.should have_selector('table td')
+      # how do i put a variable within here. "#{artist.name} doesn't work"
+      # click_link("#{artist.name}")
+      page.should have_selector('table a')
+    end
+  
+    it 'displays a link to delete artists' do
+      visit artists_path
+      page.should have_link('delete')
+      click_link('delete')
+      (Artist.count).should eq 1
+    end
+
+    it 'displays a link to edit an artist' do
+      visit artists_path
+      page.should have_link('edit')
+      click_link('edit')
+      find('h3').should have_content('Edit an Artist')
+    end
+  end
+
+  describe 'edit artist page' do
+    it 'can edit the attributes for an instance of artist' do
+      visit edit_artist_path
+      page.should have_selector('form#edit_artist')
+      within('form#edit_artist') do
+        fill_in 'Name', with: 'Queen'
+        fill_in 'Url', with: 'HTTP://Queen.com'
+        page.should have_selector('input[type=submit]')
+        click_button('Edit Artist')
+      end
+      current_path.should eq artists_path
+    end
+  end
+
+  describe 'GET artist/show' do
+    context 'from artists#index view' do
+      it 'displays an artists info' do
+        visit artists_path
+        page.should have_selector('table a')
+        # click_link("#{artist.name}")
+        visit artist_path
+        save_and_open_page
+        find('h3').should have_content('Artist URL')
+      end
     end
   end
 
